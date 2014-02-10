7902fb7cf5ceafb0c12feb1df75af86257a156ee
mathdu07 <mathdu07.minecraft@gmail.com>
2014-02-10 20:50:03 +0100

    Add GPL header to missing files

--- src/main/java/fr/mathdu07/nbtviewer/nms/NBTEntity.java
+++ src/main/java/fr/mathdu07/nbtviewer/nms/NBTEntity.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright © 2014 mathdu07 <mathdu07.minecraft@gmail.com>
+ * 
+ * This file is part of NBT Viewer.
+ * 
+ * NBT Viewer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * NBT Viewer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with NBT Viewer. If not, see <http://www.gnu.org/licenses/>.
+ */
 package fr.mathdu07.nbtviewer.nms;
 
 import java.lang.reflect.Method;
